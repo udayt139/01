9a52df8e3d5709cd4caa96eb25afcf6b0c8df8c8
udayt139 <uday@totient.co.in>
2019-10-11 12:27:03 +0530

    1

---
+++ Test Suites/Offers Regression.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Offers Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3f35d27d-f2d5-4c71-a7e8-fedd62875bbc</testSuiteGuid>
+   <testCaseLink>
+      <guid>0337b04c-573a-4717-81bc-8921b3edd76f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Scenarios/Regression/Login/GSM Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f8538b4a-a178-40a3-ab7c-4f1ad806862b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Scenarios/Regression/Offers/Create Offer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>89f9f788-1496-4c0d-83e6-f8929c836b87</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Scenarios/Regression/Offers/Edit Offer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>189903c4-26d8-424e-81a1-ed962ce6c6d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Scenarios/Regression/Offers/Delete Offer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7c5d13cf-7660-47d1-a451-770c41da16f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Scenarios/Regression/Logout/GSM Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
